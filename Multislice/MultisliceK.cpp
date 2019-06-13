12251ddc8ada0f75df8414f03b6028b96a6d8fda
timg021 <timg021@gmail.com>
2019-06-13 16:44:12 +1000

    Removed MultisliceCpp.cpp inherited code from MultisliceK.cpp (separated the two projects).

--- Multislice/MultisliceK.cpp
+++ Multislice/MultisliceK.cpp
@@ -1,75 +1,31 @@
 // MultisliceCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-//#define TEG_MULTISLICE //if undefined, Kirkland's multislice code is used instead
+#define TEG_MULTISLICE //if undefined, Kirkland's multislice code is used instead
 
 #include <stdio.h>
-#include "XA_ini.h"
-#include "XA_data.h"
-#include "XAHWave.h"
 
-#ifdef TEG_MULTISLICE
-#include "ProjSpheres.h"
-#else
+#include "XA_ini.h"
+
 #include "autosliccmd.h"
-#endif // TEG_MULTISLICE
 
 using namespace xar;
 
 int main(void)
 {
 	string outfilename("C:\\Users\\tgureyev\\Downloads\\aaa.grd");
-	const size_t nangles = 1;
-
-#ifdef TEG_MULTISLICE
-	const size_t nslices = 1; 
-	const size_t nx(512), ny(512);
-	vector<double> vHead(5); //analogue of Wavehead2D
-	const double energ(1.0); //E in keV
-	const double LengthScale(1.0); //scaling factor for length-type parameters
-	vHead[0] = 12.398E-4 / energ; //wl in microns
-	vHead[1] = -LengthScale; //ylo
-	vHead[2] = LengthScale; //yhi
-	vHead[3] = -LengthScale; //xlo
-	vHead[4] = LengthScale; //xhi
-	const double zlo(-LengthScale), zhi(LengthScale);
-
-	const size_t nSpheres(10);
-	XArray1D<double> R_in(nSpheres), x_in(nSpheres), y_in(nSpheres), z_in(nSpheres), x_in_i(nSpheres), z_in_i(nSpheres);
-
-	R_in[0] = 0.3 * LengthScale;
-	for (size_t i = 1; i < nSpheres; i++) R_in[i] = 0.1 * LengthScale;
-	
-	x_in[0] = 0;
-	for (size_t i = 1; i < nSpheres; i++) x_in[i] = pow(-1, i) * double(i % 3 + 1) * 0.2 * LengthScale;
-
-	y_in[0] = -0.5 * LengthScale;
-	for (size_t i = 1; i < nSpheres; i++) y_in[i] = 0.5 * LengthScale;
-
-	z_in[0] = 0;
-	for (size_t i = 1; i < nSpheres; i++) z_in[i] = pow(-1, int(i / 5)) * double(i % 3 + 1) * 0.2 * LengthScale;
-
-	vector<dcomplex> nc(nSpheres);
-	vector<double> beta_in(nSpheres), delta_in(nSpheres);
-	double BetaOrder = 1.E-5, DeltaOrder = 1.e-6; //scaling factors for beta and delta
-	for (size_t i = 0; i < nSpheres; i++) beta_in[i] = double(i % 5 + 1) * BetaOrder;
-	for (size_t i = 0; i < nSpheres; i++) delta_in[i] = double(i % 5 + 1) * DeltaOrder;
-	for (size_t i = 0; i < nSpheres; i++)
-		nc[i] = dcomplex(1.0 - delta_in[i], beta_in[i]);
-#endif
+	const size_t nangles = 180;
 
 	size_t i_dot = outfilename.rfind('.');
 	size_t nfield_length = (nangles == 1) ? 1 : 1 + size_t(log10(double(nangles - 1))); //maximum number of digits in the output file name
 	char ndig[8];
 	sprintf(ndig, "%zd", nfield_length); //convert the calculated maximum number of digits into a string, e.g. 3 into "3"
 	string myformat = "%0" + string(ndig) + "d"; //construct format string for inserting 0-padded numbers into file names - see usage below
+	string autoslictxt[numaslicpars], strAngle;
 
 	double angle;
 	char buffer[128], bufangle[128];
 	string outfilename_i;
-#ifndef TEG_MULTISLICE
-	string autoslictxt[numaslicpars], strAngle;
-#endif
 	try
 	{
 		for (size_t i = 0; i < nangles; i++)
@@ -80,15 +36,7 @@ int main(void)
 			outfilename_i.insert(i_dot, buffer);
 			angle = PI * double(i) / double(nangles);
 			sprintf(bufangle, "%f", angle); strAngle = bufangle;
-#ifdef TEG_MULTISLICE
-			for (size_t k = 0; k < nSpheres; k++)
-			{
-				x_in_i[k] = x_in[k] * cos(angle) + z_in[k] * sin(angle); //x - position of the centre of the internal sphere at the i - th rotation step
-				z_in_i[k] = -x_in[k] * sin(angle) + z_in[k] * cos(angle); //z - position of the centre of the internal sphere at the i - th rotation step
-			}
-			MultisliceSphereNF(nx, ny, vHead, nc, R_in, x_in_i, y_in, z_in_i, zlo, zhi, nslices, outfilename_i.data());
-#else
-			//!!! Now instead of TEG function MultisliceSphereNF() we will call Kirkland's autoslic at each angle
+			//Here we call Kirkland's autoslic at each angle
 			//!!! One has to define at least parameters no. 0, 2, 10, 11, 13, 25 and 26 in the list below
 			autoslictxt[0] = "1.Name_of_file_with_input_atomic_coordinates_in_x,y,z_format: 3j6kLysLesKirck.xyz";
 			autoslictxt[1] = "2.Replicate_unit_cell_by_NCELLX,NCELLY,NCELLZ: 1 1 1";
@@ -117,7 +65,6 @@ int main(void)
 			autoslictxt[24] = "25.Sample_(xz)_rotation_angle_in_radians: " + strAngle;
 			autoslictxt[25] = "26.Coordinates_of_the_centre_of_rotation_in_Angstroms: 56.526327 56.526327";
 			autosliccmd(autoslictxt);
-#endif
 		}
 	}
 	catch (std::exception& E)
