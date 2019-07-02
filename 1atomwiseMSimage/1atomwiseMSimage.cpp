4069fd4836d54adc9a4ed24fdc14ef56718ae394
timg021 <timg021@gmail.com>
2019-07-02 17:41:55 +1000

    The new project 1atomwiseMSimage compiles and executes correctly.

--- 1atomwiseMSimage/1atomwiseMSimage.cpp
+++ 1atomwiseMSimage/1atomwiseMSimage.cpp
@@ -1,114 +1,223 @@
 // 1atomwiseMSimage.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <stdio.h>
+#include <thread>
+#include <chrono>
 
 #include "XA_ini.h"
+#include "XArray2D.h"
 #include "XA_data.h"
-#include "XAHWave.h"
-#include <stdio.h>
 
-include "autoslic.h"
+#include "autosliccmd.h"
+#undef TEG_MULTITHREADED // this program is intended to run single-threaded only
+
+#include "pdb.h"
+extern "C" int pdb_read(pdbdata* ppd, int nfiletype, char* pdbfile);
+extern "C" int xyz_Kirck_write1(int iii, pdbdata* ppd, char* outfile, char* cfileinfo, double ctblength, double xminx0, double yminy0, double zminz0);
+
+extern xar::XArray2D<float> intenTot; // accumulated intensity data - created in 1autosliccmd.cpp, wrtten into a file at the end in this module
 
 using namespace xar;
 
+unsigned int Counter_Obj::counter = 0; // active worker thread counter (this main thread is not counted)
+bool Counter_Obj::isUpdated = false; // update status of the counter object
+
 int main(void)
 {
-	string outfilename("C:\\Users\\tgureyev\\Downloads\\Cpp\\aaa.grd");
-	const size_t nslices = 2;
-	const size_t nangles = 1;
-	const size_t nx(512), ny(512);
-	vector<double> vHead(5); // analogue of Wavehead2D
-	const double energ(1.0); // E in keV
-	const double LengthScale(1.0); //scaling factor for length-type parameters
-	vHead[0] = 12.398E-4 / energ; //wl in microns
-	vHead[1] = -LengthScale; //ylo
-	vHead[2] = LengthScale; //yhi
-	vHead[3] = -LengthScale; //xlo
-	vHead[4] = LengthScale; //xhi
-	const double zlo(-LengthScale), zhi(LengthScale);
-
-	const size_t nSpheres(10);
-	XArray1D<double> R_in(nSpheres), x_in(nSpheres), y_in(nSpheres), z_in(nSpheres), x_in_i(nSpheres), z_in_i(nSpheres);
-
-	R_in[0] = 0.3 * LengthScale;
-	for (size_t i = 1; i < nSpheres; i++) R_in[i] = 0.1 * LengthScale;
-
-	x_in[0] = 0;
-	for (size_t i = 1; i < nSpheres; i++) x_in[i] = pow(-1, i) * double(i % 3 + 1) * 0.2 * LengthScale;
-
-	y_in[0] = -0.5 * LengthScale;
-	for (size_t i = 1; i < nSpheres; i++) y_in[i] = 0.5 * LengthScale;
-
-	z_in[0] = 0;
-	for (size_t i = 1; i < nSpheres; i++) z_in[i] = pow(-1, int(i / 5)) * double(i % 3 + 1) * 0.2 * LengthScale;
-
-	vector<dcomplex> nc(nSpheres);
-	vector<double> beta_in(nSpheres), delta_in(nSpheres);
-	double BetaOrder = 1.E-5, DeltaOrder = 1.e-6; //scaling factors for beta and delta
-	for (size_t i = 0; i < nSpheres; i++) beta_in[i] = double(i % 5 + 1) * BetaOrder;
-	for (size_t i = 0; i < nSpheres; i++) delta_in[i] = double(i % 5 + 1) * DeltaOrder;
-	for (size_t i = 0; i < nSpheres; i++)
-		nc[i] = dcomplex(1.0 - delta_in[i], beta_in[i]);
-
-	size_t i_dot = outfilename.rfind('.');
-	size_t nfield_length = (nangles == 1) ? 1 : 1 + size_t(log10(double(nangles - 1))); //maximum number of digits in the output file name
-	char ndig[8];
-	sprintf(ndig, "%zd", nfield_length); //convert the calculated maximum number of digits into a string, e.g. 3 into "3"
-	string myformat = "%0" + string(ndig) + "d"; //construct format string for inserting 0-padded numbers into file names - see usage below
-
-	double angle;
-	char buffer[128];
-	string outfilename_i;
-	string autoslictxt[25];
+#ifdef TEG_MULTITHREADED
+	Counter_Obj thread_counter; // increments the thread counter on construction and decrements it on destruction
+#endif // TEG_MULTITHREADED
+	vector<string> autoslictxt(29); // 29 is the current number of input parameters; if it is changed, the corresponding changes need to be applied in autosliccmd.cpp too.
 
 	try
 	{
+		printf("\nStarting 1atomwiseMSimage program ...");
+		// read input parameter file
+		FILE* ff0 = fopen("MsctKirkland.txt", "rt");
+		if (!ff0) throw std::exception("Error: cannot open parameter file MsctKirkland.txt.");
+
+		char cline[1024], ctitle[1024], cparam[1024];
+
+		// The ordering of these parameters is 'historic', it can be changed, but then the corresponding changes need to be applied in autosliccmd.cpp too.
+		fgets(cline, 1024, ff0); // 1st line - comment
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 2nd line: Input_file_with_atomic_numbers_and_coordinates_in_XYZ_format
+		autoslictxt[0] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 3rd line: Output_GRD/GRC_filename
+		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading line 3 of input parameter file.");
+		string outfilename(cparam); // output filename stub
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 4th line: Output_intensity(0),_phase(1)_or_complex_amplitude(2)
+		autoslictxt[27] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 5th line: Use_multislice(0),_projection(1)_or_1st_Born(2)_approximation
+		autoslictxt[25] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 6th line: Incident__electron_beam_energy_in_keV
+		autoslictxt[10] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 7th line: Wavefunction_size_in_pixels,_Nx,Ny
+		autoslictxt[11] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 8th line: Slice_thickness_in_Angstroms
+		autoslictxt[13] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 9th line: Propagation(defocus)_distance_for_exit_wave_in_Angstroms !!!!!!!!!!!!!! CHANGE
+		autoslictxt[26] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 10th line: Total_CT_rotation_span_in_degrees
+		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading line 10 of input parameter file.");
+		double angle_max = atof(cparam); // total rotation span in degrees 
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 11th line: Number_of_CT_rotation_angles
+		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading line 11 of input parameter file.");
+		size_t nangles = (size_t)atoi(cparam); // total rotation span in degrees 
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 12th line: Number_of_worker_threads_to_launch_in_CT_simulation_mode
+		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading line 12 of input parameter file.");
+		unsigned int ncores = (unsigned int)atoi(cparam) + 1; // number of threads to use (expected to be equal to the number of cores) 
+		fgets(cline, 1024, ff0); // 13st line - comment
+		fgets(cline, 1024, ff0); // 14st line - comment
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 15th line: Replicate_unit_cell_by_NCELLX,NCELLY,NCELLZ
+		autoslictxt[1] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 16th line: Do_you_want_to_include_partial_coherence
+		autoslictxt[3] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 17th line: ____Illumination_angle_min,_max_in_mrad
+		autoslictxt[4] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 18th line: ____Spherical_aberration_Cs3,_Cs5_in_mm
+		autoslictxt[5] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 19th line: ____Defocus_mean,_standard_deviation,_and_sampling_size_in_Angstroms
+		autoslictxt[6] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 20th line: ____Objective_aperture_in_mrad
+		autoslictxt[7] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 21th line: Do_you_want_to_start_from_previous_result
+		autoslictxt[8] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 22nd line: ____Name_of_file_to_start_from
+		autoslictxt[9] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 23nd line: Crystal_tilt_x,y_in_mrad
+		autoslictxt[12] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 24th line: Do_you_want_to_record_the_(real,imag)_value_of_selected_beams_vs._thickness
+		autoslictxt[14] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 25th line: ____Name_of_file_for_beams_info
+		autoslictxt[15] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 26th line: ____Number_of_beams
+		autoslictxt[16] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 27th line: Do_you_want_to_include_thermal_vibrations
+		autoslictxt[17] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 28th line: ____Temperature_in_degrees_K
+		autoslictxt[18] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 29th line: ____Number_of_configurations_to_average_over
+		autoslictxt[19] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 30th line: ____Initial_seed_for_random_number_generator
+		autoslictxt[20] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 31st line: Do_you_want_to_output_intensity_vs._depth_cross_section
+		autoslictxt[21] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 32nd line: ____Name_of_file_to_get_depth_profile_image
+		autoslictxt[22] = cline;
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 33rd line: ____Y_position_of_depth_cross_section_in_Angstroms
+		autoslictxt[23] = cline;
+		fclose(ff0); // close input parameter file
+
+		// find out the number of CPU cores available in the computer
+		//unsigned int ncores = std::thread::hardware_concurrency();
+		//printf("\nNumber of CPU cores detected: %d", ncores);
+
+		char inpdbfile[256]; // input file name
+		char infiletype[256]; // input file type
+		char strctblength[256]; // CT qube side length
+		char outfile[256]; // output file name
+		char cfileinfo[256]; // freeform info line
+
+		// read PDB file translate parameter file
+		FILE* ffpar = fopen("pdb.txt", "rt");
+		if (!ffpar) throw std::exception("Error: cannot open parameter file pdb.txt.");
+		fgets(inpdbfile, 256, ffpar); strtok(inpdbfile, "\n");
+		fgets(infiletype, 256, ffpar); strtok(infiletype, "\n");
+		fgets(strctblength, 256, ffpar); strtok(strctblength, "\n");
+		fgets(outfile, 256, ffpar); strtok(outfile, "\n");
+		fgets(cfileinfo, 256, ffpar); strtok(cfileinfo, "\n");
+		fclose(ffpar);
+
+		int nfiletype = 0;
+		nfiletype = (int)atof(infiletype);
+		double ctblength = 0;
+		ctblength = atof(strctblength);
+
+		// read PDB or VestaXYZ file to get atomic coordinates
+		pdbdata pd;
+		pdb_read(&pd, nfiletype, inpdbfile);
+		double xmin, xmax, ymin, ymax, zmin, zmax;
+		xmin = xmax = pd.adata[0].x;
+		ymin = ymax = pd.adata[0].y;
+		zmin = zmax = pd.adata[0].z;
+		for (size_t i = 1; i < pd.natoms; i++)
+		{
+			if (pd.adata[i].x < xmin) xmin = pd.adata[i].x;
+			else if (pd.adata[i].x > xmax) xmax = pd.adata[i].x;
+			if (pd.adata[i].y < ymin) ymin = pd.adata[i].y;
+			else if (pd.adata[i].y > ymax) ymax = pd.adata[i].y;
+			if (pd.adata[i].z < zmin) zmin = pd.adata[i].z;
+			else if (pd.adata[i].z > zmax) zmax = pd.adata[i].z;
+		}
+		double xminx0 = 0.5 * ctblength - 0.5 * (xmax - xmin) - xmin;
+		double yminy0 = 0.5 * ctblength - 0.5 * (ymax - ymin) - ymin;
+		double zminz0 = 0.5 * ctblength - 0.5 * (zmax - zmin) - zmin;
+		if (xminx0 < -xmin || yminy0 < -ymin || zminz0 < -zmin)
+			throw std::exception("Error: sample's x, y or z extent is larger than the defined CT sample qube side length!!!");
+
+		// define output filename
+		nangles = pd.natoms; // !!! define "number of angles" equal to natoms
+		size_t i_dot = outfilename.rfind('.');
+		size_t nfield_length = (nangles == 1) ? 1 : 1 + size_t(log10(double(nangles - 1))); //maximum number of digits in the output file name
+		char ndig[8];
+		sprintf(ndig, "%zd", nfield_length); //convert the calculated maximum number of digits into a string, e.g. 3 into "3"
+		string myformat = "%0" + string(ndig) + "d"; //construct format string for inserting 0-padded numbers into file names - see usage below
+
+		char buffer[128], bufangle[128];
+		string strAngle, outfilename_i;
+		//double angle;
+		//double angle_step = angle_max / 180.0 * PI / double(nangles); // rotation step in radians
+
+		// start the execution timer
+		std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
+
+		// start the cycle over projection angles
 		for (size_t i = 0; i < nangles; i++)
 		{
-			printf("Angle = %zd\n", i);
+			printf("\nAngle = %zd", i);
+			
+			//write new XYZ file with a single atom no. i
+			xyz_Kirck_write1((int)i, &pd, outfile, cfileinfo, ctblength, xminx0, yminy0, zminz0);
+
 			outfilename_i = outfilename;
 			sprintf(buffer, myformat.data(), i);
 			outfilename_i.insert(i_dot, buffer);
-			angle = PI * double(i) / double(nangles);
-			//for (size_t k = 0; k < nSpheres; k++)
-			//{
-			//	x_in_i[k] = x_in[k] * cos(angle) + z_in[k] * sin(angle); //x - position of the centre of the internal sphere at the i - th rotation step
-			//	z_in_i[k] = -x_in[k] * sin(angle) + z_in[k] * cos(angle); //z - position of the centre of the internal sphere at the i - th rotation step
-			//}
-			//MultisliceSphereNF(nx, ny, vHead, nc, R_in, x_in_i, y_in, z_in_i, zlo, zhi, nslices, outfilename_i.data());
-			//!!! Now instead of TEG function MultisliceSphereNF() we will call Kirkland's autoslic at each angle
-			autoslictxt[0] = "1.Name_of_file_with_input_atomic_coordinates_in_x, y, z_format: si3n4.xyz";
-			autoslictxt[1] = "2.Replicate_unit_cell_by_NCELLX, NCELLY, NCELLZ : 1 1 1";
-			autoslictxt[2] = "3.Name_of_file_to_get_binary_output_of_multislice_result : si3n4_45.tif";
-			autoslictxt[3] = "4.Do_you_want_to_include_partial_coherence : 0";
-			autoslictxt[4] = "5.____Illumination_angle_min, _max_in_mrad : 0.0 0.0";
-			autoslictxt[5] = "6.____Spherical_aberration_Cs3, _Cs5_in_mm : 0.0 0.0";
-			autoslictxt[6] = "7.____Defocus_mean, _standard_deviation, _and_sampling_size_in_Angstroms : 0.0 0.0 0.0";
-			autoslictxt[7] = "8.____Objective_aperture_in_mrad : 0.0";
-			autoslictxt[8] = "9.Do_you_want_to_start_from_previous_result : 0";
-			autoslictxt[9] = "10.____Name_of_file_to_start_from : 0";
-			autoslictxt[10] = "11.Incident_beam_energy_in_keV : 200.0";
-			autoslictxt[11] = "12.Wavefunction_size_in_pixels, _Nx, Ny : 1024 1024";
-			autoslictxt[12] = "13.Crystal_tilt_x, y_in_mrad : 0.0 0.0";
-			autoslictxt[13] = "14.Slice_thickness_in_Angstroms : 1.375";
-			autoslictxt[14] = "15.Do_you_want_to_record_the_(real, imag)_value_of_selected_beams_vs._thickness : 0";
-			autoslictxt[15] = "16.____Name_of_file_for_beams_info : 0";
-			autoslictxt[16] = "17.____Number_of_beams : 0";
-			autoslictxt[17] = "18.Do_you_want_to_include_thermal_vibrations : 0";
-			autoslictxt[18] = "19.____Type_the_temperature_in_degrees_K : 0.0";
-			autoslictxt[19] = "20.____Type_number_of_configurations_to_average_over : 0";
-			autoslictxt[20] = "21.____Type_initial_seed_for_random_number_generator : 0";
-			autoslictxt[21] = "22.Do_you_want_to_output_intensity_vs._depth_cross_section : 0";
-			autoslictxt[22] = "23.____Type_name_of_file_to_get_depth_profile_image : 0";
-			autoslictxt[23] = "24.____Type_y_position_of_depth_cross_section_in_Angstroms : 0.0";
-			autoslictxt[24] = "25.Sample_(xz)_rotation_angle_in_radians : 0.785398163397";
-
-			autosliccmd(autoslictxt);
+			//angle = angle_step * double(i);
+			sprintf(bufangle, "%f", 0.0); strAngle = bufangle;
+			//Here we call Kirkland's autoslic at each angle
+			autoslictxt[2] = "3.Name_of_file_to_get_binary_output_of_multislice_result: " + outfilename_i;
+			autoslictxt[24] = "25.Sample_(xz)_rotation_angle_in_radians: " + strAngle;
+			autoslictxt[28] = "29.Copy(0)_or_initialize(1)_FFTW_plan: 1"; // the first thread must initialize the FFTW plan, subsequent ones can copy it
+#ifdef TEG_MULTITHREADED
+			if (i > 0) autoslictxt[28] = "29.Copy(0)_or_initialize(1)_FFTW_plan: 0";
+			thread_counter.SetUpdated(false);
+			std::thread threadObj(autosliccmd, autoslictxt);
+			if (i == 0) threadObj.join(); // we need to let the first worker thread finish execution, so that it can create the FFTW "plan" to be shared  with other threads
+			if (threadObj.joinable()) threadObj.detach(); // if we don't do this, threadObj will call Terminate() on the attached thread when the threadObj goes out of scope
+			while (!thread_counter.GetUpdated() || thread_counter.GetCount() >= ncores)
+				std::this_thread::sleep_for(std::chrono::milliseconds(10)); // we allow ncores of threads to be launched
+#else
+			autosliccmd(autoslictxt); // single-threaded execution mode
+#endif // TEG_MULTITHREADED
 		}
+
+		// save the accumulated total intensity file
+		intenTot += 1.0f;
+		XArData::WriteFileGRD(intenTot, outfilename.c_str(), xar::eGRDBIN);
+
+#ifdef TEG_MULTITHREADED
+		while (thread_counter.GetCount() > 1)
+			std::this_thread::sleep_for(std::chrono::milliseconds(100)); // wait for all worker threads to finish or fail
+#endif // TEG_MULTITHREADED
+		std::chrono::system_clock::time_point end_time = std::chrono::system_clock::now();
+		printf("\nMain program finished. Execution time = %I64d s.", std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count());
+
 	}
 	catch (std::exception& E)
 	{
 		printf("\n!!!Exception: %s\n", E.what());
 	}
 
+	printf("\nPress any key to exit..."); getchar();
 	return 0;
 }
---
+++ 1atomwiseMSimage/pdb_run1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include "pdb.h"
+
+
+int pdb_read(pdbdata* ppd, int nfiletype, char* pdbfile)
+{
+	//pdbdata pd;
+	printf("\nReading atomic coordinates file ...\n");
+	pdbdata_init(ppd);
+	if (nfiletype)
+	{
+		printf("\nReading input XYZ file in Vesta export format ...\n");
+		data_from_VestaXYZfile(pdbfile, ppd); // read Vesta export XYZ file
+	}
+	else
+	{
+		printf("\nReading input PDB file ...\n");
+		read_pdb(pdbfile, ppd); // read PDB file
+	}
+	//print_pdb_data(&pd);
+
+	printf("Finished!");
+	return 0;
+}
+
+
+int xyz_Kirck_write1(int iii, pdbdata* ppd, char* outfile, char* cfileinfo, double ctblength, double xminx0, double yminy0, double zminz0)
+{
+	int i, ia, j;
+
+	printf("\nWriting output XYZ file in Kirkland's format ...\n");
+	FILE* ff = fopen(outfile, "wt");
+	fprintf(ff, "%s\n", cfileinfo); // free-form file info line
+	fprintf(ff, "%f %f %f\n", ctblength, ctblength, ctblength); // ctblength is written as the unit cell dimension
+
+	for (i = 0; i < ppd->natoms; i++)
+	{
+		if (i != iii) continue; // only read output for the single selected atom
+
+		// translate the element symbol into atomic number (weight)
+		ia = 0; j = 0;
+		while (isspace(ppd->adata[i].element[j])) j++; // skip leading white spaces
+		if (j > 2)
+		{
+			printf("!!!Too many white spaces %s!!!", ppd->adata[i].element);
+			return -1;
+		}
+		if (ppd->adata[i].element[j] == 'H') ia = 1; // H = hydrogen
+		else if (ppd->adata[i].element[j] == 'C')
+		{
+			ia = 6; // C = carbon
+			if (ppd->adata[i].element[j + 1] == 'A') ia = 20; // CA = calcium
+			else if (ppd->adata[i].element[j + 1] == 'O') ia = 27; // CO = cobalt
+		}
+		else if (ppd->adata[i].element[j] == 'N')
+		{
+			ia = 7; // N = nitrogen
+			if (ppd->adata[i].element[j + 1] == 'A') ia = 11; // NA = sodium
+			else if (ppd->adata[i].element[j + 1] == 'I') ia = 28; // NI = nickel
+		}
+		else if (ppd->adata[i].element[j] == 'O') ia = 8; // O = oxygen
+		else if (ppd->adata[i].element[j] == 'P') ia = 15; // P = posphorus
+		else if (ppd->adata[i].element[j] == 'S') ia = 16; // S = sulphur
+		else if (ppd->adata[i].element[j] == 'F') ia = 26; // assuming FE = ferrum
+		else if (ppd->adata[i].element[j] == 'M') ia = 25; // assuming MN = manganese
+		else if (ppd->adata[i].element[j] == 'Z') ia = 30; // assuming ZN = zinc
+		else if (ia == 0)
+		{
+			printf("!!!Unknown atom type %s!!!", ppd->adata[i].element);
+			return -1;
+		}
+		// TEG: we make all coordinates non-negative, as it seems that Kirkland's software expects this,
+		// and we also centre each of XYZ coordinates within the interval [0, ctblength], so that the sample can remain within the cube during rotation
+		fprintf(ff, "%d %f %f %f %f\n", ia, ppd->adata[i].x + xminx0, ppd->adata[i].y + yminy0, ppd->adata[i].z + zminz0, ppd->adata[i].occupancy);
+	}
+	fprintf(ff, "%i\n\n\n", -1);
+	fclose(ff);
+
+	printf("Finished!");
+
+	return 0;
+}
