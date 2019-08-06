20b69fdcd0591d258f1e1282c2d2924946f255be
TimGu <TimGu@DESKTOP-MI2890M>
2019-08-06 20:46:25 +1000

    First executing version of BigBangCT - no meaningful code yet, just a demonstration of working 3D FFT using fftw library.

--- BigBangCT/BigBangCT.cpp
+++ BigBangCT/BigBangCT.cpp
@@ -2,26 +2,27 @@
 //
 
 #include <iostream>
+#include <complex.h>
 #include <fftw3.h>
 #include <XArray3D.h>
 
 int main()
 {
+	index_t nx(4), ny(4), nz(4);
     std::cout << "Hello World!\n";
-	xar::XArray3D<double> aaa(4, 4, 4, 1.0);
+	xar::XArray3D<float> aaa(nx, ny, nz, 1.0);
 
-
-	fftw_plan_dft_r2c_3d(2, 2, 2, &aaa[0][0][0], &aaa[0][0][0], FFTW_ESTIMATE);
-
+	printf("\nBefore FFT:");
+	for (index_t i = 0; i < nx; i++)
+		for (index_t j = 0; j < ny; j++)
+			for (index_t k = 0; k < nz; k++)
+				printf("\ni = %zd, j = %zd, k = %zd, arr[i,j,k] = %g", i, j, k, aaa[i][j][k]);
+	fftwf_plan fp = fftwf_plan_dft_r2c_3d(2, 2, 2, &aaa[0][0][0], (fftwf_complex*)& aaa[0][0][0], FFTW_ESTIMATE);
+	fftwf_execute(fp);
+	printf("\nAfter FFT:");
+		for (index_t i = 0; i < nx; i++)
+			for (index_t j = 0; j < ny; j++)
+				for (index_t k = 0; k < nz; k++)
+					printf("\ni = %zd, j = %zd, k = %zd, arr[i,j,k] = %g", i, j, k, aaa[i][j][k]);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
