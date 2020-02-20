5dd8863f0d88e9036462705d4a25ec8acda544e2
Timur Gureyev <timg021@gmail.com>
2020-02-20 17:32:46 +1100

    In the process of extending PbiSAXS to CT mode.

--- PbiSAXS/PbiSAXS.cpp
+++ PbiSAXS/PbiSAXS.cpp
@@ -21,48 +21,79 @@ int main()
 		printf("\nStarting PbiSAXS program ...");
 
 		//string strfilepath = "C:\\Users\\gur017\\Downloads\\Temp\\";
-		string strfilepath = "C:\\Users\\tgureyev\\Downloads\\Temp\\";
-		string infile = "img4096_1m.grd"; // input file with an in-line projection image
-		string outfile = "SAXS0" + infile; // output file with SAXS image
+		string strfilepath = "C:\\Users\\tgureyev\\Downloads\\Temp\\LysLesPhaseCT200keV_900rot\\";
+		string infile = "phase.grd", infile_i; // input file with an in-line projection image
+		string outfile = "saxs" + infile, outfile_i; // output file with SAXS image
 		XArray2D<float> xaimagein; // input in-line projection image array
 		XArray2D<float> xaobjtie; // TIE-Hom retrieved intensity array
 		XArray2D<float> xapha; // phase
 		XArray2D<float> xaint; // intensity
 
-		double wl = 1.e-4; // X-ray wavelength in microns
-		double defocus = 1.e+6; // defocus distance in microns
-		double delta2beta = 300; // delta/beta
+		index_t nangles = 900;
+		double angle_range = 180.0;
+		double angle_step = angle_range / nangles, angle;
 
-		// read the in-line projection image from input file
-		XArData::ReadFileGRD(xaimagein, (strfilepath + infile).c_str(), wl);
-		xaint = xaimagein; // make a temporary copy of the input image
+		char buffer[128];
+		// create formatting string to add properly formatted indexes at the end of the output file names
+		size_t i_dot = infile.rfind('.'), o_dot = outfile.rfind('.'), nfieldB_length;
+		char ndig[8];
+		string myformat("");
+		if (nangles > 1)
+		{
+			nfieldB_length = 1 + size_t(log10(double(nangles - 1))); //maximum number of digits corresponding to angles in the output file name
+			sprintf(ndig, "%zd", nfieldB_length); //convert the calculated maximum number of digits corresponding to angles into a string, e.g. 3 into "3"
+			myformat += "%0" + string(ndig) + "d"; //construct format string for inserting 0-padded angle indexes into file names - see usage below
+		}
 
-		// do TIE-Hom phase retrieval
-		XA_2DTIE<float> xatie;
-		xatie.DP(xaint, delta2beta, defocus);
-		xaobjtie = xaint; // save the TIE-Hom retrieved intensity for later use
-		//XArData::WriteFileGRD(xaobjtie, (strfilepath + "TIE" + infile).c_str(), eGRDBIN);
+		double wl = 2.5e-6; // X-ray wavelength in microns
+		double defocus = 0.015; // defocus distance in microns
+		double delta2beta = 0.1; // delta/beta
 
-		// Fresnel-propagate the TIE-Hom retrieved object-plane complex amplitude forward to the image plane
-		xapha = xaint;
-		xapha.Log();
-		xapha *= float(0.5 * delta2beta);
-		xaint ^= 0.5; // convert intensity into real amplitude
-		XArray2D<fcomplex> xacamp;
-		MakeComplex(xaint, xapha, xacamp, true);
-		XArray2DFFT<float> xafft2(xacamp);
-		xafft2.Fresnel(defocus);
-		Abs2(xacamp, xaint);
+		for (index_t i = 0; i < nangles; i++)
+		{
+			angle = angle_step * double(i);
+			printf("\nAngle = %f", angle);
 
-		// Subtract the retrieved-repropagated image from the original image, take the inverse Laplacian and divide by I0
-		xaimagein -= xaint; // this is the SAXS component of the image
-		//XArData::WriteFileGRD(xaimagein, (strfilepath + "SAXS1" + infile).c_str(), eGRDBIN);
-		xatie.DP(xaimagein, delta2beta, defocus); // regularized inverse Laplacian
-		//XArData::WriteFileGRD(xaimagein, (strfilepath + "SAXS0TIE" + infile).c_str(), eGRDBIN);
-		xaimagein /= xaobjtie; // this is the SAXS component of the object
+			// generate input and output file names
+			infile_i = infile;
+			outfile_i = outfile;
+			sprintf(buffer, myformat.data(), i);
+			infile_i.insert(i_dot, buffer);
+			outfile_i.insert(o_dot, buffer);
+			printf("\nInput file = %s", infile_i.c_str());
+			printf("\nOutput file = %s", outfile_i.c_str());
 
-		// write the result to output file
-		XArData::WriteFileGRD(xaimagein, (strfilepath + outfile).c_str(), eGRDBIN);
+			// read the in-line projection image from input file
+			XArData::ReadFileGRD(xaimagein, (strfilepath + infile_i).c_str(), wl);
+			xaint = xaimagein; // make a temporary copy of the input image
+
+			// do TIE-Hom phase retrieval
+			XA_2DTIE<float> xatie;
+			xatie.DP(xaint, delta2beta, defocus);
+			xaobjtie = xaint; // save the TIE-Hom retrieved intensity for later use
+			//XArData::WriteFileGRD(xaobjtie, (strfilepath + "TIE" + infile_i).c_str(), eGRDBIN);
+
+			// Fresnel-propagate the TIE-Hom retrieved object-plane complex amplitude forward to the image plane
+			xapha = xaint;
+			xapha.Log();
+			xapha *= float(0.5 * delta2beta);
+			xaint ^= 0.5; // convert intensity into real amplitude
+			XArray2D<fcomplex> xacamp;
+			MakeComplex(xaint, xapha, xacamp, true);
+			XArray2DFFT<float> xafft2(xacamp);
+			xafft2.Fresnel(defocus);
+			Abs2(xacamp, xaint);
+
+			// Subtract the retrieved-repropagated image from the original image, take the inverse Laplacian and divide by I0
+			xaimagein -= xaint; // this is the SAXS component of the image
+			//XArData::WriteFileGRD(xaimagein, (strfilepath + "SAXS1" + infile_i).c_str(), eGRDBIN);
+			xatie.DP(xaimagein, delta2beta, defocus); // regularized inverse Laplacian
+			//XArData::WriteFileGRD(xaimagein, (strfilepath + "SAXS0TIE" + infile_i).c_str(), eGRDBIN);
+			xaimagein /= xaobjtie; // this is the SAXS component of the object
+
+			// write the result to output file
+			XArData::WriteFileGRD(xaimagein, (strfilepath + outfile_i).c_str(), eGRDBIN);
+		}
 	}
 	catch (std::exception & E)
 	{
