18892e7f36b5f849b2d7426dbc50eb6dbdd822d2
Timur Gureyev <timg021@gmail.com>
2020-09-04 13:02:27 +1000

    In the process of implementing the option of a set of different rotation angles in PhaseRetrieval  program

--- PhaseRetrieval/PhaseRetrieval.cpp
+++ PhaseRetrieval/PhaseRetrieval.cpp
@@ -31,7 +31,15 @@ int main()
 		if (!ff0) throw std::exception("Error: cannot open parameter file PhaseRetrieval.txt.");
 		fgets(cline, 1024, ff0); // 1st line - comment
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 1. Input defocus distances in Angstroms
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 1. CT rotation span in degrees and number of rotation angles
+		if (sscanf(cline, "%s %s %s", ctitle, cparam, cparam1) != 3) throw std::exception("Error reading rotation span and number of rotations from input parameter file.");
+		double angle_span = atof(cparam); // total rotation span in degrees 
+		index_t nangles = (index_t)atoi(cparam); // number of rotation steps 
+		double angle_step = angle_span / 180.0 * PI / double(nangles); // rotation step in radians
+		// !!!Later on, input parameter lines 1 and 2 (rotation angles and defocus distances) should be replaced by a single name of an input text (CSV-type) file
+		// which will contain one row per rotational position, each row containing a rotation angle followed by an arbitrary number of defocus distances at that angle
+
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 2. Input defocus distances in Angstroms
 		std::vector<size_t> vwhite(0); // vector of white spaces separating different defocus distances (there should be exactly one white space before each defocus distance and no spaces at the end)
 		for (size_t i = 1; i < strlen(cline); i++) if (isspace(cline[i])) vwhite.push_back(i); // count the number of different defocus distances in the input parameter file
 		size_t ndefocus = vwhite.size(); // number of detected defocus distances
@@ -49,21 +57,21 @@ int main()
 			printf("%g ", vdefocus[j]);
 		}
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 2. Input_filename_base_of_defocus_series_of_the_sample_in_GRD_format
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 3. Input_filename_base_of_defocus_series_of_the_sample_in_GRD_format
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading defocus series file name base from input parameter file.");
 		string filenamebaseIn = cparam;
 		printf("\nDefocus series file name base = %s", filenamebaseIn.c_str());
 		vector<string> vinfilenames;
 		FileNames(1, ndefocus, filenamebaseIn, vinfilenames); // create vector of input filenames
 		
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 3. Wavelength in Angstroms
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 4. Wavelength in Angstroms
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading wavelength from input parameter file.");
-		double wl = atof(cparam); // wavelength in input file units (usually, Angstroms). Unfortunately, it is not saved in the GRD files
+		double wl = atof(cparam); // wavelength in Angstroms
 		printf("\nWavelength = %g (A)", wl);
 		if (wl < 0 || wl > 1)
 			throw std::exception("Error: wavelength value appears to be wrong.");
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 4. Objective aperture in mrad
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 5. Objective aperture in mrad
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading objective aperture from input parameter file.");
 		double aobj = atof(cparam); 
 		printf("\nObjective aperture = %g (mrad)", aobj);
@@ -71,7 +79,7 @@ int main()
 			throw std::exception("Error: objective aperture value appears to be wrong.");
 		double k2maxo = pow(aobj * 0.001f / wl, 2.0); // Fourier space bandwidth
 		
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 5. Spherical aberrations Cs3 and Cs5 in mm
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 6. Spherical aberrations Cs3 and Cs5 in mm
 		if (sscanf(cline, "%s %s %s", ctitle, cparam, cparam1) != 3) throw std::exception("Error reading spherical aberrations from input parameter file.");
 		double Cs3 = atof(cparam);
 		double Cs5 = atof(cparam1);
@@ -79,21 +87,21 @@ int main()
 		Cs3 *= 1.e+7; // mm --> Angstroms
 		Cs5 *= 1.e+7; // mm --> Angstroms
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 6. Maximal number of IWFR iterations
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 7. Maximal number of IWFR iterations
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading maximal number of iterations from input parameter file.");
 		int kmax = atoi(cparam);
 		printf("\nMaximal number of iterations = %d", kmax);
 		if (kmax < 1)
 			throw std::exception("Error: the maximal number of iterations should be >= 1.");
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 7. Minimal phase reconstruction error
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 8. Minimal phase reconstruction error
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading minimal phase reconstruction error from input parameter file.");
 		double epsilon = atof(cparam);
 		printf("\nMinimal phase reconstruciton error = %g", epsilon);
 		if (epsilon < 0)
 			throw std::exception("Error: minimal phase reconstruction error must be non-negative.");
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 8. Output defocus distances min max and step in Angstroms
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 9. Output defocus distances min max and step in Angstroms
 		if (sscanf(cline, "%s %s %s %s", ctitle, cparam, cparam1, cparam2) != 4) throw std::exception("Error reading output defocus distances from input parameter file.");
 		double outdefocus_min = atof(cparam); // minimum output defocus in Angstroms 
 		double outdefocus_max = atof(cparam1); // maximum output defocus in Angstroms 
@@ -110,11 +118,11 @@ int main()
 			printf("%g ", voutdefocus[j]);
 		}
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 9. Output intensity(0), phase(1) or complex_amplitude(2)
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 10. Output intensity(0), phase(1) or complex_amplitude(2)
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading output files format from input parameter file.");
 		int noutformat = atoi(cparam);
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 10. Output file name base in GRD or GRC format
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 11. Output file name base in GRD or GRC format
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading output file name base from input parameter file.");
 		string filenamebaseOut = cparam;
 		printf("\nOutput file name base = %s", filenamebaseOut.c_str());
@@ -139,9 +147,9 @@ int main()
 			throw std::exception("Error: unknown value for output file format in input parameter file.");
 		}
 		vector<string> voutfilenames;
-		FileNames(1, noutdefocus, filenamebaseOut, voutfilenames); // create vector of output filenames
+		FileNames(nangles, noutdefocus, filenamebaseOut, voutfilenames); // create vector of output filenames
 
-		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 11. Number of parallel threads
+		fgets(cline, 1024, ff0); strtok(cline, "\n"); // 12. Number of parallel threads
 		if (sscanf(cline, "%s %s", ctitle, cparam) != 2) throw std::exception("Error reading number of parallel threads from input parameter file.");
 		int nThreads = atoi(cparam);
 		printf("\nNumber of parallel threads = %d", nThreads);
@@ -154,125 +162,131 @@ int main()
 
 		//************************************ end reading input parameters from file
 
-		// define main work objects
-		double dtemp; // auxilliary variable
-		double ssej(0.0), ssejm1(0.0); // current and previous average reconstruction errors
-		vector<double> verr(ndefocus); // reconstruction errors in individual defocus planes
-		vector<XArray2D<double>> vint0(ndefocus), vint(ndefocus); // initial and iterated defocused intensities
-		vector<XArray2D<dcomplex>> vcamp(ndefocus); // defocused complex amplitudes
-		XArray2D<dcomplex> campOut; // complex amplitude in the plane z=0
-		vector<double> vint0_L1(ndefocus); // L1 norms of the initial defocused intensities
-
-		// read input GRD files and create the initial complex amplitudes
-		for (index_t n = 0; n < ndefocus; n++)
+		// start of cycle over rotation angles
+		for (index_t na = 0; na < nangles; na++) 
 		{
-			XArData::ReadFileGRD(vint0[n], vinfilenames[n].c_str(), wl);
-			vint0_L1[n] = vint0[n].Norm(eNormL1);
-			if (vint0_L1[n] == 0) throw std::exception("Error: input intensity file is empty");
-			vint0[n] ^= 0.5; // intensity --> real amplitude
-		}
+			// define main work objects
+			double dtemp; // auxilliary variable
+			double ssej(0.0), ssejm1(0.0); // current and previous average reconstruction errors
+			vector<double> verr(ndefocus); // reconstruction errors in individual defocus planes
+			vector<XArray2D<double>> vint0(ndefocus), vint(ndefocus); // initial and iterated defocused intensities
+			vector<XArray2D<dcomplex>> vcamp(ndefocus); // defocused complex amplitudes
+			XArray2D<dcomplex> campOut; // complex amplitude in the plane z=0
+			vector<double> vint0_L1(ndefocus); // L1 norms of the initial defocused intensities
 
-		// start point of IWFR iterations
-		for (int k = 0; k < kmax; k++)
-		{
-			// apply initial or newly reconstructed phases (the second case is equal to restoring the original moduli)
-			// and backpropagate each defocused amplitude to the plane z = 0
+			// read input GRD files and create the initial complex amplitudes
+			//@@@@@@@@@@@@@@@@@ these need to be changed to take into account different rotation angles
+			for (index_t n = 0; n < ndefocus; n++)
+			{
+				XArData::ReadFileGRD(vint0[n], vinfilenames[n].c_str(), wl);
+				vint0_L1[n] = vint0[n].Norm(eNormL1);
+				if (vint0_L1[n] == 0) throw std::exception("Error: input intensity file is empty");
+				vint0[n] ^= 0.5; // intensity --> real amplitude
+			}
+
+			// start point of IWFR iterations
+			for (int k = 0; k < kmax; k++)
+			{
+				// apply initial or newly reconstructed phases (the second case is equal to restoring the original moduli)
+				// and backpropagate each defocused amplitude to the plane z = 0
+				#pragma omp parallel for
+				for (int n = 0; n < ndefocus; n++)
+				{
+					try
+					{
+						if (k == 0)	MakeComplex(vint0[n], 0.0, vcamp[n], true); // apply initial zero phases
+						else // apply phases obtained on the previous iteration
+							for (index_t j = 0; j < vcamp[n].GetDim1(); j++)
+								for (index_t i = 0; i < vcamp[n].GetDim2(); i++)
+								{
+									dtemp = abs(vcamp[n][j][i]);
+									if (dtemp) vcamp[n][j][i] *= vint0[n][j][i] / dtemp;
+									else MakeComplex(vint0[n], 0.0, vcamp[n], true);
+								}
+						xar::XArray2DFFT<double> xafft(vcamp[n]);
+						xafft.Fresnel(-vdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z = 0
+					}
+					catch (std::exception& E)
+					{
+						printf("\n\n!!!Exception: %s\n", E.what());
+						break;
+					}
+				}
+
+				// average backpropagated complex amplitudes
+				campOut = vcamp[0];
+				for (index_t n = 1; n < ndefocus; n++) campOut += vcamp[n];
+				campOut /= double(ndefocus);
+
+				// forward propagate the averaged complex amplitude to the defocus planes
+				#pragma omp parallel for
+				for (int n = 0; n < ndefocus; n++)
+				{
+					try
+					{
+						vcamp[n] = campOut;
+						xar::XArray2DFFT<double> xafft(vcamp[n]);
+						xafft.Fresnel(vdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z = z[n]
+						Abs(vcamp[n], vint[n]);
+						vint[n] -= vint0[n];
+						verr[n] = pow(vint[n].Norm(eNormL2), 2.0) / vint0_L1[n];
+					}
+					catch (std::exception& E)
+					{
+						printf("\n\n!!!Exception: %s\n", E.what());
+						break;
+					}
+				}
+
+				// calculate the current reconstruction error and
+				// if the difference with the previous error is smaller than the defined minimum
+				// or, if the error started to increase, interrupt the iterations
+				ssej = 0.0;
+				for (index_t n = 0; n < ndefocus; n++) ssej += verr[n];
+				ssej /= double(ndefocus);
+				printf("\nIteration number %d; SSE_aver error difference = %g\n", k, ssejm1 - ssej);
+				for (index_t n = 0; n < ndefocus; n++) printf("SSE(%zd) = %g ", n, verr[n]);
+				if (k && (ssejm1 - ssej) < epsilon) break;
+				else ssejm1 = ssej;
+			}
+			// end point of IWFR iterations
+
+			// calculate and save output defocused images
+			printf("\n");
 			#pragma omp parallel for
-			for (int n = 0; n < ndefocus; n++)
+			for (int n = 0; n < noutdefocus; n++)
 			{
 				try
 				{
-					if (k == 0)	MakeComplex(vint0[n], 0.0, vcamp[n], true); // apply initial zero phases
-					else // apply phases obtained on the previous iteration
-						for (index_t j = 0; j < vcamp[n].GetDim1(); j++)
-							for (index_t i = 0; i < vcamp[n].GetDim2(); i++)
-							{
-								dtemp = abs(vcamp[n][j][i]);
-								if (dtemp) vcamp[n][j][i] *= vint0[n][j][i] / dtemp;
-								else MakeComplex(vint0[n], 0.0, vcamp[n], true);
-							}
-					xar::XArray2DFFT<double> xafft(vcamp[n]);
-					xafft.Fresnel(-vdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z = 0
+					XArray2D<dcomplex> campOutn(campOut);
+					xar::XArray2DFFT<double> xafft(campOutn);
+					xafft.Fresnel(voutdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z_out[n]
+					XArray2D<double> ipOut;
+					// write the defocused intensity, phase or complex amplitude into output file
+					printf("\nOutput defocus distance = %g; output file = %s", voutdefocus[n], voutfilenames[n].c_str());
+					switch (noutformat)
+					{
+					case 0: // intensity out
+						Abs2(campOutn, ipOut);
+						XArData::WriteFileGRD(ipOut, voutfilenames[n].c_str(), eGRDBIN);
+						break;
+					case 1: // phase out
+						CArg(campOutn, ipOut); // @@@@ should be replaced by CArg later (after CArg is debugged)
+						XArData::WriteFileGRD(ipOut, voutfilenames[n].c_str(), eGRDBIN);
+						break;
+					case 2: // complex amplitude out
+						XArData::WriteFileGRC(campOutn, voutfilenames[n].c_str(), eGRCBIN);
+						break;
+					}
 				}
 				catch (std::exception& E)
 				{
 					printf("\n\n!!!Exception: %s\n", E.what());
 					break;
 				}
-			}
+			} // end of cycle over output defocus distances
 
-			// average backpropagated complex amplitudes
-			campOut = vcamp[0];
-			for (index_t n = 1; n < ndefocus; n++) campOut += vcamp[n];
-			campOut /= double(ndefocus);
-
-			// forward propagate the averaged complex amplitude to the defocus planes
-			#pragma omp parallel for
-			for (int n = 0; n < ndefocus; n++)
-			{
-				try
-				{
-					vcamp[n] = campOut;
-					xar::XArray2DFFT<double> xafft(vcamp[n]);
-					xafft.Fresnel(vdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z = z[n]
-					Abs(vcamp[n], vint[n]);
-					vint[n] -= vint0[n];
-					verr[n] = pow(vint[n].Norm(eNormL2), 2.0) / vint0_L1[n];
-				}
-				catch (std::exception& E)
-				{
-					printf("\n\n!!!Exception: %s\n", E.what());
-					break;
-				}
-			}
-
-			// calculate the current reconstruction error and
-			// if the difference with the previous error is smaller than the defined minimum
-			// or, if the error started to increase, interrupt the iterations
-			ssej = 0.0;
-			for (index_t n = 0; n < ndefocus; n++) ssej += verr[n];
-			ssej /= double(ndefocus);
-			printf("\nIteration number %d; total error SSE_aver = %g\n", k, ssej);
-			for (index_t n = 0; n < ndefocus; n++) printf("SSE(%zd) = %g ", n, verr[n]);
-			if (k && (ssejm1 - ssej) < epsilon) break;
-			else ssejm1 = ssej;
-		} 
-		// end point of IWFR iterations
-
-		// calculate and save output defocused images
-		printf("\n");
-		#pragma omp parallel for
-		for (int n = 0; n < noutdefocus; n++)
-		{
-			try
-			{
-				XArray2D<dcomplex> campOutn(campOut);
-				xar::XArray2DFFT<double> xafft(campOutn);
-				xafft.Fresnel(voutdefocus[n], false, k2maxo, Cs3, Cs5); // propagate to z_out[n]
-				XArray2D<double> ipOut;
-				// write the defocused intensity, phase or complex amplitude into output file
-				printf("\nOutput defocus distance = %g; output file = %s", voutdefocus[n], voutfilenames[n].c_str());
-				switch (noutformat)
-				{
-				case 0: // intensity out
-					Abs2(campOutn, ipOut);
-					XArData::WriteFileGRD(ipOut, voutfilenames[n].c_str(), eGRDBIN);
-					break;
-				case 1: // phase out
-					CArg(campOutn, ipOut); // @@@@ should be replaced by CArg later (after CArg is debugged)
-					XArData::WriteFileGRD(ipOut, voutfilenames[n].c_str(), eGRDBIN);
-					break;
-				case 2: // complex amplitude out
-					XArData::WriteFileGRC(campOutn, voutfilenames[n].c_str(), eGRCBIN);
-					break;
-				}
-			}
-			catch (std::exception& E)
-			{
-				printf("\n\n!!!Exception: %s\n", E.what());
-				break;
-			}
-		}
+		} // end of cycle over rotation angles
 
 	}
 	catch (std::exception& E)
